d2cf1add19f2ef8c9b3c158543fac8deaced2195
shivanshu-semwal <semwalshivanshu@gmail.com>
2021-04-10 12:30:54 +0530

    added map reduce program

---
+++ Driver.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Driver {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		Job j = new Job();
+		j.setJobName("freq counter");
+		j.setJarByClass(Driver.class);
+		j.setMapperClass(MyMapper.class);
+		j.setReducerClass(MyReducer.class);
+		j.setOutputKeyClass(Text.class);
+		j.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(j, new Path(args[0]));
+		FileOutputFormat.setOutputPath(j, new Path(args[1]));
+		System.exit(j.waitForCompletion(true) ? 0 : 1);
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Hadoop Practical
 
+- [Mapper Class](./MyMapper.java)
+- [Reducer Class](./MyReducer.java)
+- [Driver](./Driver.java)
+- [Input File](./input.txt)
+- [Output File](./output/part-r-00000)
+
+### Execution
+
+```sh
+# storing class path for hadoop imports file
+H_PATH=$(hadoop classpath)
+
+# compiling
+javac -cp $H_PATH *.java
+
+# creating jar file
+jar -cvf count.jar *.class 
+
+# uploading the local input file to HDFS
+hadoop fs -put input.txt
+
+# executing mapReduce program
+hadoop jar count.jar Driver input.txt output
+
+# listing files in the output folder
+hadoop fs -ls output
+
+# previewing the output
+hadoop fs -cat output/part-r-00000
+```
